4737ac92d952b90a45075ca1da52c67025310abf
MuhammadMaaz7 <maazawan100@gmail.com>
2025-02-18 00:45:22 +0500

    Graph creation

--- game.py
+++ game.py
@@ -1,50 +1,80 @@
 from node import Node
-file = open("words_alpha.txt", "r" )
-data = file.read()
-words = data.split("\n") #Extracting All the words from txt
-# print(words)
 
-# a = Node("a",None,[])
-# print(a.state)
 
-def ValidWord(input):
+#To Check if the word exists in the dictionary
+def ValidWord(input,words):
     for word in words:
         if(input == word):
-            print("Word is Valid. Word exists in Dictionary")
+            # print("Word exists in Dictionary")
             return True
     
     return False
         
+#To check if the new word is made by 1 letter transformation
 def compare(input,similar):
+    if len(input) != len(similar):
+        return False
+    
     transformationCount = 0 
     for i in range(len(input)):
         if(input[i] != similar[i]):
             transformationCount+=1
     
     if transformationCount==1:
-        print(similar)
         return True
     return False
 
 
+def userInput(words):
+    startWord = input("Enter start word: ")
+    while True:
+        if ValidWord(startWord,words) != True:
+            print("Word does not exist in Dictionary")
+            startWord = input("Enter a valid start word: ")
+        else:
+            break
 
-while True:
-    inputWord = input("Enter start word: ")
-    if ValidWord(inputWord) == True:
-        break
+    endWord = input("Enter end word: ")
+    while True:
+        if startWord == endWord:
+            print("Start and End word cannot be same.")
+            endWord = input("Enter a valid end word: ")
+        elif len(startWord) != len(endWord):
+            print("Start and End word must be of same length.")
+            endWord = input("Enter a valid end word: ")
+        elif ValidWord(endWord,words) == False:
+            print("Word does not exist in Dictionary")
+            endWord = input("Enter a valid end word: ")
+        else:
+            break
 
-while True:
-    outputWord = input("Enter end word: ")
-    if inputWord != outputWord and len(inputWord) == len(outputWord) and ValidWord(outputWord) == True:
-        break
+    return startWord,endWord
 
-dictionary = []
-
-for word in words:
-    if(len(word) == len(inputWord)):
-        dictionary.append(word)
-
-for word in dictionary:
-     compare(inputWord,word) 
-        
+#To add all the possible transformations from a word
+def addAllTransformations(currentWord, words,graph):
     
+    # 1. If word is valid, then make a Node and add it to the graph
+    # 2. add parent, actions to the node
+    for word in words:
+        if compare(currentWord,word):
+            graph[word] = Node(word,currentWord,[])
+            parent = graph[word].parent
+            graph[parent].actions.append(word)
+
+    
+def main():
+     #Extracting All the words from txt file
+    file = open("words_alpha.txt", "r" )
+    data = file.read()
+    words = data.split("\n")
+    
+    startWord,endWord = userInput(words)
+    
+    graph = {}
+    graph[startWord] = Node(startWord,None,[])
+    addAllTransformations(startWord,words,graph)
+    
+    print(graph.items())
+    
+
+main()
--- node.py
+++ node.py
@@ -1,5 +1,11 @@
 class Node:
-    def _init_(self,state,parent,actions):
+    def __init__(self,state,parent,actions):
         self.state = state
         self.parent=parent
         self.actions=actions
+        
+    def __str__(self):
+        return f"Node(word={self.state}, parent={self.parent}, actions={self.actions})"
+
+    def __repr__(self):
+        return self.__str__()
