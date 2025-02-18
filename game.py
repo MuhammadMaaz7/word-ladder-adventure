7c2a5191ba55c4b95cd2060c05f562f3808ea445
MuhammadMaaz7 <maazawan100@gmail.com>
2025-02-18 23:11:47 +0500

    cnoidsvd

--- game.py
+++ game.py
@@ -1,11 +1,11 @@
 from node import Node
+from collections import deque
 
 
 #To Check if the word exists in the dictionary
 def ValidWord(input,words):
     for word in words:
         if(input == word):
-            # print("Word exists in Dictionary")
             return True
     
     return False
@@ -52,16 +52,41 @@ def userInput(words):
 
 #To add all the possible transformations from a word
 def addAllTransformations(currentWord, words,graph):
-    
-    # 1. If word is valid, then make a Node and add it to the graph
-    # 2. add parent, actions to the node
     for word in words:
         if compare(currentWord,word):
-            graph[word] = Node(word,currentWord,[])
-            parent = graph[word].parent
-            graph[parent].actions.append(word)
-
+            
+            #Adding word if it does not exist
+            if word not in graph:
+                graph[word] = Node(word,currentWord,[])
+            
+            # if graph[word].parent != None:
+            #     parent = graph[word].parent
+                
+            # if word already exists in its actions then dont add it again
+            if word not in graph[currentWord].actions:
+                graph[currentWord].actions.append(word)
+            
+def buildGraph(startWord,endWord, words):     
+    graph ={startWord : Node(startWord,None)}
+    queue = deque([startWord]) 
     
+    while queue:
+        currentWord = queue.popleft()
+        addAllTransformations(currentWord,words,graph)
+        
+        for action in graph[currentWord].actions:
+            # if action not in graph:
+            queue.append(action)
+                
+            if action == endWord:
+                return graph
+            
+    return graph
+        
+def printGraph(graph):
+    for word, node in graph.items():
+        print(f"Word: {word}, Parent: {node.parent}, Actions: {node.actions}")
+        
 def main():
      #Extracting All the words from txt file
     file = open("words_alpha.txt", "r" )
@@ -69,14 +94,42 @@ def main():
     words = data.split("\n")
     
     #User Input
-    startWord,endWord = userInput(words)
+    # startWord,endWord = userInput(words)
+    startWord = "cat"
+    endWord = "big"
     
     #Creating Graph
-    graph = {}
-    graph[startWord] = Node(startWord,None,[])
-    addAllTransformations(startWord,words,graph)
+    graph = buildGraph(startWord,endWord,words)
     
-    print(graph.items())
+    printGraph(graph)
     
+    
+    # currentNode = graph[startWord]
+    # while (currentNode.word != endWord): 
+    #     if currentNode.actions == []:
+    #         addAllTransformations(currentNode.word,words,graph)
+    #         print(graph.items())
+    #     else:
+    #         for action in currentNode.actions:    
+    #             print("Node: ", currentNode.word, " action: ",action)  
+    #             addAllTransformations(action,words,graph)
+    #             for action in currentNode.actions:
+    #                 print("Node: ", currentNode.word, " action: ",action)
+    #                 addAllTransformations(action,words,graph)
+    #                 print(graph.items())
+    #                 currentNode = graph[currentNode.actions[action]]
+
+                
+
+  
 
 main()
+
+
+# 1. Graph from start word to end word
+# 2. consists of all paths from start to end
+# 3. Words(Nodes) should not be repeated (If a word(node) exists in graph it shouldnt be added again)
+# 4. Each node should have parent and actions
+# 5. Parent of a node cannot be updated
+# 6. Both Nodes should have action of eachother, meaning its possible to go from a cat -> bat, and also bat -> cat
+
--- node.py
+++ node.py
@@ -1,11 +1,11 @@
 class Node:
-    def __init__(self,state,parent,actions):
-        self.state = state
+    def __init__(self,word,parent=None,actions=[]):
+        self.word = word
         self.parent=parent
-        self.actions=actions
+        self.actions=actions    
         
     def __str__(self):
-        return f"Node(word={self.state}, parent={self.parent}, actions={self.actions})"
+        return f"Node(word={self.word}, parent={self.parent}, actions={self.actions})"
 
     def __repr__(self):
         return self.__str__()
