6593eb5cf8d55140cbb1aae4e17dc327f9cd02ab
MuhammadMaaz7 <maazawan100@gmail.com>
2025-02-20 00:02:31 +0500

    Manual Game

--- game.py
+++ game.py
@@ -9,6 +9,21 @@ def ValidWord(input,words):
             return True
     
     return False
+        
+#To check if the new word is made by 1 letter transformation
+def compare(input,similar):
+    if len(input) != len(similar):
+        return False
+    
+    transformationCount = 0 
+    for i in range(len(input)):
+        if(input[i] != similar[i]):
+            transformationCount+=1
+    
+    if transformationCount==1:
+        return True
+    return False
+
 
 def userInput(words):
     startWord = input("Enter start word: ")
@@ -35,32 +50,15 @@ def userInput(words):
 
     return startWord,endWord
 
-#To check if the new word is made by 1 letter transformation
-def compare(input,similar):
-    if len(input) != len(similar):
-        return False
-    
-    transformationCount = 0 
-    for i in range(len(input)):
-        if(input[i] != similar[i]):
-            transformationCount+=1
-    
-    return transformationCount
-
 #To add all the possible transformations from a word
 def addAllTransformations(currentWord, words,graph):
-    #print("Adding Tranformations for ",currentWord)
     for word in words:
-        if compare(currentWord,word) == 1:
+        if compare(currentWord,word):
             
             #Adding word if it does not exist
             if word not in graph:
                 graph[word] = Node(word,currentWord,[])
             
-            # if graph[word].parent != None:
-            #     parent = graph[word].parent
-                
-            # if word already exists in its actions then dont add it again
             if word not in graph[currentWord].actions:
                 graph[currentWord].actions.append(word)
             
@@ -69,81 +67,97 @@ def buildGraph(startWord,endWord, words):
     queue = deque([startWord]) 
     explored = []
 
+    
     while queue:
         currentWord = queue.popleft()
-        #print("Popping ",currentWord)
         if currentWord not in explored:
             addAllTransformations(currentWord,words,graph)
         
             for action in graph[currentWord].actions:
-                # if action not in graph:
-
-                #if compare(endWord,action) < len(endWord): #money stone
-                    queue.append(action)
-                # if not queue:
-                #     print("Queue is empty at ",currentWord)
-                    
-                # if action == endWord:
-                #     print("End word found----------Exiting")
-                #     return graph
+                queue.append(action)
+                
             
             explored.append(currentWord)
-        
-
+            
         if endWord in explored:
-            print("Explored ",explored)
             return graph
-
+            
     return graph
+
+def pathExists(startWord,endWord,graph):
+    Explored = set()
+    queue = deque([startWord])
+    while queue:
+        word = queue.popleft()
+        if word == endWord:
+            return True
+        Explored.add(word)
+        for action in graph[word].actions:
+            if action not in Explored:
+                Explored.add(action)
+                queue.append(action)
+                
+    return False
         
+def playManualGame(startWord,endWord,graph):
+    currentNode = graph[startWord]
+    moves = 0
+    path = []
+    
+    while currentNode.word != endWord:
+        if currentNode.actions == []:
+            addAllTransformations(currentNode.word,graph.keys(),graph)
+        print(f"Current Word: {currentNode.word}, Actions: {currentNode.actions}")
+        nextWord = input("Enter the next word: ")
+        while nextWord not in currentNode.actions:
+            nextWord = input("Invalid Word, Enter Again:")
+        
+        path.append(nextWord)
+        moves+=1
+        currentNode = graph[nextWord]
+        
+        if currentNode.word == endWord:
+            print("Congratulations, You won, Score: ",moves)
+            print("Path: ",path)
+            return True
+
 def printGraph(graph):
     for word, node in graph.items():
         print(f"Word: {word}, Parent: {node.parent}, Actions: {node.actions}")
-        print("---------------")
         
 def main():
      #Extracting All the words from txt file
     file = open("words_alpha.txt", "r" )
     data = file.read()
     words = data.split("\n")
+    dictionary = []
     
     #User Input
-    # startWord,endWord = userInput(words)
-    startWord = "stone"
-    endWord = "money"
     
-    dictionary = []
+    
+    startWord,endWord = userInput(words)
+    
     for word in words:
-        #print("For Word",word)
         if len(word) == len(startWord):
             dictionary.append(word)
-    print("Dictionary",dictionary)
     
     #Creating Graph
     graph = buildGraph(startWord,endWord,dictionary)
     
-    printGraph(graph)
+    # printGraph(graph)
     
+    while pathExists(startWord,endWord,graph) == False:
+        print("No path exists between these words")
+        startWord,endWord = userInput(words)
+        
+        for word in words:
+            if len(word) == len(startWord):
+                dictionary.append(word)
     
-    # currentNode = graph[startWord]
-    # while (currentNode.word != endWord): 
-    #     if currentNode.actions == []:
-    #         addAllTransformations(currentNode.word,words,graph)
-    #         print(graph.items())
-    #     else:
-    #         for action in currentNode.actions:    
-    #             print("Node: ", currentNode.word, " action: ",action)  
-    #             addAllTransformations(action,words,graph)
-    #             for action in currentNode.actions:
-    #                 print("Node: ", currentNode.word, " action: ",action)
-    #                 addAllTransformations(action,words,graph)
-    #                 print(graph.items())
-    #                 currentNode = graph[currentNode.actions[action]]
-
-                
-
+        
+        graph = buildGraph(startWord,endWord,dictionary)
+    playManualGame(startWord,endWord,graph)
   
-
 main()
 
 
@@ -153,4 +167,3 @@ main()
 # 4. Each node should have parent and actions
 # 5. Parent of a node cannot be updated
 # 6. Both Nodes should have action of eachother, meaning its possible to go from a cat -> bat, and also bat -> cat
-
